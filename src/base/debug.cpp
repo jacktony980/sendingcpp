4b88f14975984fdf455cd743ddf26918fc031118
Tusooa Zhu <tusooa@kazv.moe>
2022-01-29 00:10:35 -0500

    Convert to SPDX copyright indicator

--- src/base/debug.cpp
+++ src/base/debug.cpp
@@ -1,20 +1,7 @@
 /*
- * Copyright (C) 2020-2021 Tusooa Zhu <tusooa@vista.aero>
- *
  * This file is part of libkazv.
- *
- * libkazv is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * libkazv is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with libkazv.  If not, see <https://www.gnu.org/licenses/>.
+ * SPDX-FileCopyrightText: 2020-2021 Tusooa Zhu <tusooa@kazv.moe>
+ * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
 #include "libkazv-config.hpp"
